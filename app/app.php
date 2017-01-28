c0059af4bb0dfbff99ed0fb2da4ae016ca276adb
Frey <freyandhy@gmail.com>
2017-01-28 14:28:27 +0700

    using eloquent

--- app/app.php
+++ app/app.php
@@ -7,11 +7,31 @@ require __DIR__ . '/../vendor/autoload.php';
 $app = new \Slim\App([
     'settings' => [
         'displayErrorDetails' => true,
+        'db' => [
+            'driver' => 'mysql',
+            'host' => 'localhost',
+            'database' => 'slimauth',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+        ]
     ],
 ]);
 
-// grab the container
+// Grab the container
 $container = $app->getContainer();
+
+// Start Settings Eloquent
+$capsule = new \Illuminate\Database\Capsule\Manager;
+$capsule->addConnection($container['settings']['db']);
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
+
+$container['db'] = function($container) use ($capsule) {
+    return $capsule;
+};
+// End Settings Eloquent
 
 $container['view'] = function ($container) {
     // create new slim view twig instance
